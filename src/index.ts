b77cfb45ed4c7a485b447afafd1fc167f736aed7
khaled Elshokri <khaledelshokri28@outlook.com>
2025-01-03 20:47:04 -0500

    How to declare functions and setup the tsconfig to detect potential errors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+// Functions
 
-// PascalCase
+function calculateTax(income: number, taxYear = 2022): number{
+    if(taxYear < 2022)
+        return income * 1.2;
+    else
+    return income * 1.3;
+}
 
-const enum Size {Small = 1, Medium, Large};
-
-let mySize: Size = Size.Medium;
-
-console.log(mySize);
+calculateTax(10_000);
