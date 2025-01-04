e47beb3770605f02a51ca3819ac7005bab054b36
khaled Elshokri <khaledelshokri28@outlook.com>
2025-01-03 23:10:05 -0500

    Used type intersection and Union types

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
-// Objects
-
-
-type Employee = {
-    
-    readonly id: number,
-    name: string,
-    retire: (date: Date) => void
-}
-let employee: Employee = {
-    id: 1,
-    name: 'Khaled',
-    retire: (date: Date) => {
-        console.log(date);
-    }
+// Type intersection
+type Draggable = {
+    drag: () => void
 };
+
+type Resizable = {
+    resize: () => void
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+};
+
+// Union types
+function kgtoLbs(weight: number | string): number{
+
+    // Narowing
+    if(typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+
+}
+
+kgtoLbs(10);
+kgtoLbs("10kg")
